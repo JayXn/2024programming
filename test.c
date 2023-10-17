3e9b073f0dd4f793155cc654607e7b04fd2a6302
JayXn <xja940129@gmail.com>
2023-10-17 14:39:37 +0800

    保存

--- test.c
+++ test.c
@@ -1,45 +1,9 @@
-#include<stdio.h>
-#include<math.h>
-#include<string.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
+#define ARRAYSIZE 1000
 
-int main(){
-  char a[1001];
-  
-  while(true){
-    scanf("%s", a);
-    if((a[0]-'0') == 0){
-      break;
-    }
+int main()
+{
 
-    int od = 0;
-    int even = 0;
-    for(int i = 0 ; i < strlen(a) ; i++){
-      if(i % 2 == 0){
-        even = even + (a[i]-'0');
-      }
-      else{
-        od = od + (a[i]-'0');
-      }
-    }
-
-
-    int substraction = sqrt((od - even) * (od - even));
-
-
-    if(substraction == 0 || substraction%11 == 0){
-      for(int i = 0 ; i < strlen(a) ; i++){
-        printf("%d", a[i]-'0');
-      }
-      printf(" is a multiple of 11.\n");
-    }
-    else{
-      for(int i = 0 ; i < strlen(a) ; i++){
-        printf("%d", a[i]-'0');
-      }
-      printf(" is not a multiple of 11.\n");
-    }
-  }
 }
-
-
