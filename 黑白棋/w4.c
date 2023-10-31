b660dae216bc103efda749a1096dd80efff47c47
JayXn <xja940129@gmail.com>
2023-10-31 23:39:41 +0800

    1031

--- 黑白棋/w4.c
+++ 黑白棋/w4.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 #define CHESSSIZE 8
+#define SPACE 0
+#define BLACK 1
+#define WHITE 2
+#define IN_CHESS(x,y) (x >= 0 && x <= 7 && y >= 0 && y <= 7)  
 
 int x, y, dx, dy;
 int color;
 int chess[CHESSSIZE][CHESSSIZE];
+int CanPut[CHESSSIZE][CHESSSIZE];
+
+
+void Initial_CanPut(){
+for (int i = 0; i < CHESSSIZE; i++){
+    for (int j = 0; j < CHESSSIZE; j++){
+      CanPut[i][j] = 0;
+    }
+  }
+}
+
 
 void inputArray(int n)
 {
@@ -17,15 +32,17 @@ void inputArray(int n)
       chess[i][j] = piece;
     }
   }
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+      CanPut[i][j] = 0;
+    }
+  }
+
 }
 
-
-
-int Search(int x_cell, int y_cell)
-{
+int SearchAndRecord(int chess[8][8], int CanPut[8][8], int x_cell, int y_cell, int color){
   int x = x_cell;
   int y = y_cell;
-
   for(int dy = -1 ; dy <= 1 ; dy++ ){
     for(int dx = -1 ; dx <= 1 ; dx++){
       if(color != chess[y + dy][x + dx] && chess[y + dy][x + dx] != 0){
@@ -34,8 +51,8 @@ int Search(int x_cell, int y_cell)
         while(IN_CHESS(x,y)){
           x = x + dx;
           y = y + dy;
-          if(chess[x][y] == color){
-            return 1;
+          if(chess[y][x] == color){
+            CanPut[y][x] == 1;
           }
         }
       }
@@ -44,27 +61,33 @@ int Search(int x_cell, int y_cell)
   return 0;
 }
 
-
-
+void ShowCanPut(){
+  printf("黑子可下的位子：\n");
+  for(int j = 0 ; j < CHESSSIZE ; j++){
+    for(int i = 0 ; i < CHESSSIZE ; i++){
+      if(CanPut[j][i] == 1){
+        printf("(%d, %d), ", i, j);
+      }
+    }
+  }
+  printf("\n");
+}
 
 
 int main()
 {
-
-
   while (1)
   {
     inputArray(CHESSSIZE);
 
     scanf("%d %d", &x, &y);
 
-    Search(x,y);
+    SearchAndRecord(chess[8][8], CanPut[8][8], x, y, BLACK);
+    ShowCanPut();
+    InitialCan_Put();
 
-    if(Search(x,y) == 0){
-      printf("(%d,%d)能下 color=%d 的值\n",x,y,color); 
-    }
-    else{
-      printf("(%d,%d)不能下 color=%d 的值\n",x,y,color); 
-    }
+    SearchAndRecord(chess[8][8], CanPut[8][8], x, y, WHITE);
+    ShowCanPut();
+    InitialCan_Put();
   }
 }
