9159a716400f4bd795ca5b3c7f9bf366fb27a0ed
JayXn <xja940129@gmail.com>
2023-11-22 09:50:47 +0800

    1122

---
+++ HEADLINES/ctype.c
@@ -0,0 +1,19 @@
+/*https://tw.gitbook.net/c_standard_library/ctype_h.html*/
+
+//example：int isalnum(int c);      檢查是否為數字
+
+
+//example：int isalpha(int c);      檢查是否為字母
+
+
+//example：int iscntrl(int c);       
+
+
+
+//example：
+
+
+//example：
+
+
+//example：
