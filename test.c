aa83900748241edde8e929ea0ec65d005a058474
JayXn <xja940129@gmail.com>
2023-12-06 09:12:57 +0800

    1206

--- test.c
+++ test.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 
-int bui[10000] = {0};
-
-
 int main() {
-    int L, H, R;
-    while(scanf("%d %d %d", &L, &H, &R) != EOF){
-        for(int i = L - 1 ; i < R ; i++){
-            if(bui[i] < H) bui[i] = H;
+    int SizeA, SizeB, num;
+    scanf("%d %d %d", &SizeA, &SizeB, &num);
+    int matrix[SizeA][SizeB];
+    for(int i = 0 ; i < SizeA ; i++)
+        for(int j = 0 ; j < SizeB ; j++)
+            matrix[i][j] = 0;
+
+    while(num > 0){
+        int x, y, value;
+        scanf("%d %d %d", &y, &x, &value);
+
+        matrix[y][x] = value;
+
+        num--;
+    }
+
+
+    for(int i = 0 ; i < SizeA ; i++){
+        for(int j = 0 ; j < SizeB ; j++){
+            printf("%d ", matrix[i][j]);
         }
+        printf("\n");
     }
-
-    for(int i = 0 ; i < 10000 ; i++){
-        if(bui[i] != bui[i + 1])
-            printf("%d %d", i, bui[i]);
-    }
-
-
 
     return 0;
 }
