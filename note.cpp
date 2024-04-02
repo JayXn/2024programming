5edd75cf01a220e163ced997830121135a0f81fa
JayXn <xja940129@gmail.com>
2024-04-03 00:51:35 +0800

    0403

---
+++ note.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+   void foo() {
+    cout << "A" << endl;
+  }
+};
+
+class B : public A {
+public:
+  void foo() {
+    cout << "B" << endl;
+  }
+};
+
+int main()
+{
+  /*A a;
+  a.foo();
+  B b;
+  b.foo();
+  b.A::foo();*/
+  A *a = new B;
+  a->foo();
+}
