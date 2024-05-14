bdd5c9f7b6eab2608903637f3aab5d6045fcb51a
JayXn <xja940129@gmail.com>
2024-05-14 23:28:03 +0800

    0514

--- test.cpp
+++ test.cpp
@@ -1,165 +1,104 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-class LinkedList {
-  public:
-    LinkedList():head(nullptr){}
-    ~LinkedList() {
-      Node* current = head;
-      while (current) {
-          Node* next = current->getNext();
-          delete current;
-          current = next;
-      }
-    }
+class Point{
+private:
+  float x;
+  float y;
 
-  public:
-    class Node {
-    public:
-       Node(): next(nullptr), data(0) {}
-       Node(int d): next(nullptr), data(d) {}
-       Node(Node* n): next(n), data(0) {}
-       Node(Node* n, int d): next(n), data(d) {}
-       int data;
-       Node* next;
+public:
+  Point(): x(0), y(0){};
+  Point(float a, float b) : x(a), y(b){};
 
-    public:
-      Node *&getNext(){
-        return next;
-      }
-      int &getData(){
-        return data;
-      }
-    };
-
-    class iterator {
-    public:
-      Node * it;
-      iterator(Node * p) : it(p) {}
-
-      iterator &operator++(){
-        it = it -> getNext();
-        return *this;
-      }
-      bool operator==(iterator i){
-        return it == i.it;
-      }
-      bool operator!=(iterator i){
-        return it != i.it;
-      }
-      int &operator*(){
-        return it->getData();
-      }
-    };
-
-    iterator begin(){
-      return iterator(head);
-    }
-    iterator end(){
-      return iterator(NULL);
-    }
-
-    bool addFromHead(int d) {
-      Node *newnode = new Node(d);
-      newnode -> next = head;
-      head = newnode;
-      return true;
-    }
-    bool addFromTail(int d) {
-      Node *newnode = new Node(d);
-      if(!head){
-        head = newnode;
-        return true;
-      }
-      else{
-        Node *temp = head;
-        while(temp -> getNext()){
-          temp = temp -> getNext();
-        }
-        temp -> getNext() = newnode;
-        return true;
-      }
-      return false;
-    }
-    bool removeFromHead() {
-      if(!head){
-        return false;
-      }
-      Node *temp = head;
-      head = head -> getNext();
-      delete temp;
-      return true;
-    }
-    bool removeFromTail() {
-      if(!head){
-        return false;
-      }
-      if(head -> getNext() == nullptr){
-         delete head;
-         head = nullptr;
-         return true;
-      }
-      Node *temp = head;
-      while(temp -> getNext() -> getNext()){
-        temp = temp -> getNext();
-      }
-      delete temp -> getNext();
-      temp -> next = nullptr;
-      return true;
-    }
-
-    size_t getSize() const {
-      size_t size = 0;
-      Node * temp = head;
-      while(temp){
-        size++;
-        temp = temp -> getNext();
-      }
-      return size;
-    }
-   
-    friend ostream& operator<<(ostream& o, LinkedList *l) {
-      size_t size = l -> getSize();
-      int output_times = 0;
-      if(size == 0){
-        cout << "{}" << endl;
-        return o;
-      }
-      if(size == 1){
-        cout << "{" << l->head->data << "}" << endl;
-        return o;
-      }
-      cout << "{";
-      for_each(l -> begin() , l -> end() , 
-        [&output_times , &size](int j){
-          if(output_times == size-1){
-            cout << j;
-          }
-          else{
-            cout << j << ", ";
-            output_times++;
-          }
-      });
-      cout << "}" << endl;
-      return o;
-    }
-  
-   protected:
-    Node* head;
+  void printPoint() {
+    cout << "(" << x << ", " << y << ")" << endl;
+  }
+  float getX(){
+    return x;
+  }
+  float setX(float c){
+    x = c;
+    return x;
+  }
+  float getY(){
+    return y;
+  }
+  float setY(float d){
+    y = d;
+    return y;
+  }
+  float getDistance(Point p){
+    return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
+  }
 };
 
-int main() {
-    LinkedList *list = new LinkedList();
-    list->addFromHead(5);
-    list->addFromHead(1);
-    list->addFromHead(2);
-    list->addFromTail(9);
-    list->addFromTail(9);
-    list->addFromTail(9);
-    cout << list << endl;
 
-    return 0;
-}
+class Segment{
+  public:
+  	Point p1;
+  	Point p2;
+  public:
+  	Segment(){
+      p1.setX(0);
+      p1.setY(0);
+      p2.setX(0);
+      p2.setY(0);
+    }
+  	Segment(Point _p1,Point _p2){
+      p1 = _p1;
+      p2 = _p2;
+    }
+    float getLength(){
+      return p1.getDistance(p2);
+    }
+};
+
+
+class Shape{
+private:
+  Segment seg[10];
+  int edgeCount;
+
+public:
+  Shape() : edgeCount(0){};
+  virtual float getArea() = 0;
+  Shape(Segment* s, int n) {
+      for (int i = 0; i < n - 1; i++) {
+        if ((s[i].p2.getX() != s[i + 1].p1.getX() || s[i].p2.getY() != s[i + 1].p1.getY())) {
+          throw invalid_argument("shape is not closed");
+        }
+      }
+      if (n < 3 || s[0].p1.getX() != s[n - 1].p2.getX() || s[0].p1.getY() != s[n - 1].p2.getY()) {
+        throw invalid_argument("At least 3 edges and first connect to last");
+      }
+      for (int i = 0; i < n; i++) {
+        seg[i] = s[i];
+      }
+      edgeCount = n;
+  }  // to construct a shape with a serial of segment. Throw an invalid_argument if the segment can not construct a close path.
+
+  float getPerimeter() {
+    float perimeter = 0;
+    for (int i = 0; i < edgeCount; i++) {
+        perimeter += seg[i].getLength();
+    }
+    return perimeter;
+  }
+
+  Segment& getSeg(int index) {
+      return seg[index];
+  }
+};
+
+class Rectangle : public Shape {
+public:
+  Rectangle(Segment *s) : Shape(s, 4){}
+  float getArea(){
+    return getSeg(0).getLength() * getSeg(1).getLength();
+  }
+};
