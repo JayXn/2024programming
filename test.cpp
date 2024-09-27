114eb1be8395f76a6d67011c60afbce0df630f0f
JayXn <xja940129@gmail.com>
2024-09-27 08:53:25 +0800

    0927

--- test.cpp
+++ test.cpp
@@ -1,39 +1,63 @@
-#include <cstdlib>
+#include <cstring>
 #include <iostream>
+
+#define SIZE 100
 
 using namespace std;
 
-template <class T>
-class Memory {
+class Queue {
  public:
-  static T** allocArray(int m, int n) {
-    T** array = new T*[m];
-
-    array[0] = new T[n];
-
-    for (int i = 1; i < m; i++) {
-      array[i] = array[0] + i * n;
-    }
-
-    return array;
+  Queue() {
+    top = 0;
+    bot = 0;  // 都設為0容易判斷陣列是否空
   }
+  int enqueue(int integer) {
+    if ((top + 1) % SIZE ==
+        bot) {  // 判斷queue full有兩種方法。1. 使用第三變數紀錄容器數量 2.
+                // 使用頭尾指標間的關係
+      return -1;
+    }
+    data[top] = integer;
+    top = (top + 1) % SIZE;
+    return 1;
+  }
+  int* dequeue() {  // 為什麼回傳值要用指標？
+    if (top == bot) {
+      return NULL;
+    }
+    int* result = &data[bot];  // 多設一個result讓bot加完後可以回傳原本的資料
+    bot = (bot + 1) % SIZE;
+    return result;
+  }
+
+ private:
+  int data[SIZE];
+  int top, bot;
 };
 
 int main() {
-  int** array = Memory<int>::allocArray(5, 10);
-
-  for (int j = 0; j < 5; j++)
-    for (int k = 0; k < 10; k++)
-      array[j][k] = j * 10 + k;
-
-  for (int j = 0; j < 5; j++) {
-    for (int k = 0; k < 10; k++)
-      cout << array[j][k] << " ";
-    cout << endl;
+  int data, *temp;
+  char command[50];
+  Queue* queue = new Queue();
+  while (1) {
+    cin >> command;
+    if (strcmp(command, "exit") == 0) {
+      break;
+    } else if (strcmp(command, "enqueue") == 0) {
+      cout << "Please input a integer data:";
+      cin >> data;
+      if (queue->enqueue(data) == 1) {
+        cout << "Successfully enqueue data " << data << " into queue." << endl;
+      } else {
+        cout << "Failed to enqueue data into queue." << endl;
+      }
+    } else if (strcmp(command, "dequeue") == 0) {
+      temp = queue->dequeue();
+      if (temp == NULL) {
+        cout << "Failed to dequeue a data from queue.\n";
+      } else {
+        cout << "Dequeue data " << *temp << " from queue." << endl;
+      }
+    }
   }
-
-  delete[] array[0];
-  delete[] array;
-
-  return 0;
 }
